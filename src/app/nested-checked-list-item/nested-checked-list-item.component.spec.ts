d86f842f367cb022e8feed7b0dbe9a32259dad7d
Boris Skert <boris.skert@gmail.com>
2021-08-11 21:01:59 +0200

    Clean up; extend tests

--- src/app/nested-checked-list-item/nested-checked-list-item.component.spec.ts
+++ src/app/nested-checked-list-item/nested-checked-list-item.component.spec.ts
@@ -22,10 +22,101 @@ describe('NestedCheckedListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NestedCheckedListItemComponent);
     component = fixture.componentInstance;
+
+    component.id = 'id2';
+    component.title = 'title 2';
+    component.description = 'description 2';
+    component.subItems = [
+      {
+        id: 'id21',
+        title: 'title 21',
+        description: 'description 21',
+        isChecked: false,
+      },
+      {
+        id: 'id22',
+        title: 'title 22',
+        description: 'description 22',
+        isChecked: false,
+      },
+      {
+        id: 'id23',
+        title: 'title 23',
+        description: 'description 23',
+        isChecked: false,
+      }
+    ];
+    component.isChecked = false;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when check one sub-item', () => {
+    beforeEach(() => {
+      const collapseButton = fixture.debugElement.nativeElement.querySelector('.test-collapse-button-id2');
+      collapseButton.click();
+
+      fixture.detectChanges();
+
+      const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id23');
+      checkBox.click();
+
+      fixture.detectChanges();
+    });
+
+    it('should show indeterminate checkbox', () => {
+      const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id2');
+      expect(checkBox.indeterminate).toBe(true);
+      expect(checkBox.checked).toBe(false);
+    });
+
+    describe('when check second sub-item', () => {
+      beforeEach(() => {
+        const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id22');
+        checkBox.click();
+
+        fixture.detectChanges();
+      });
+
+      it('should show indeterminate checkbox', () => {
+        const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id2');
+        expect(checkBox.indeterminate).toBe(true);
+        expect(checkBox.checked).toBe(false);
+      });
+
+      describe('when check third sub-item', () => {
+        beforeEach(() => {
+          const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id21');
+          checkBox.click();
+
+          fixture.detectChanges();
+        });
+
+        it('should NOT show indeterminate checkbox', () => {
+          const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id2');
+          expect(checkBox.indeterminate).toBe(false);
+          expect(checkBox.checked).toBe(true);
+        });
+      });
+    });
+
+    describe('when collapse nested list', () => {
+      beforeEach(() => {
+        const collapseButton = fixture.debugElement.nativeElement.querySelector('.test-collapse-button-id2');
+        collapseButton.click();
+
+        fixture.detectChanges();
+      });
+
+      it('should not touch indeterminate checkbox', () => {
+        const checkBox = fixture.debugElement.nativeElement.querySelector('#list-item-id2');
+        expect(checkBox.indeterminate).toBe(true);
+        expect(checkBox.checked).toBe(false);
+      });
+    });
   });
 });
